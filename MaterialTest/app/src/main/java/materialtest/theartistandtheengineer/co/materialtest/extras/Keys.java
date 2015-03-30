c81e8c549f8fb1a96cf84c5b580f3d127ff58bdf
mpcen <vccengineers12@gmail.com>
2015-03-30 08:57:55 -0400

    reloading

--- MaterialTest/app/src/main/java/materialtest/theartistandtheengineer/co/materialtest/extras/Keys.java
+++
@@ -1,8 +0,0 @@
-package materialtest.theartistandtheengineer.co.materialtest.extras;
-
-/**
- * Created by mpcen-desktop on 3/27/15.
- */
-public interface Keys {
-
-}
--- MaterialTest/app/src/main/java/materialtest/theartistandtheengineer/co/materialtest/pojo/Information.java
+++
@@ -1,9 +0,0 @@
-package materialtest.theartistandtheengineer.co.materialtest.pojo;
-
-/**
- * Created by mpcen-desktop on 2/13/15.
- */
-public class Information {
-    public int iconId;
-    public String title;
-}
